3d814a30eb6b151abdff9936ab664aef6911563f
cgfg <1991chenxi@gmail.com>
2016-10-01 17:16:33 -0500

    finish chinese character tag

--- themes/theme-nothing-special/scripts/wordcard.js
+++ themes/theme-nothing-special/scripts/wordcard.js
@@ -1,12 +1,13 @@
 hexo.extend.tag.register('wordcard', function(args, content){
   var word = args[0];
-
   var pinyin = args.slice(1).join(' ');
-  return '<div class="ui fluid card">'
+  return '<div class="ui raised card wordcard">'
     + '<div><div class="calligraphy">'
     + word
     + '</div></div>'
-    + '<div class="content pinyin"><a class="header speak">'
+    + '<div class="content pinyin"><a class="header speak" word="'
+    + word
+    + '">'
     + '<i class="sound icon"> </i>'
     + pinyin
     +'</a></div>'
